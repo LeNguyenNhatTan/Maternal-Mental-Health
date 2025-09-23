7b6e83147d1743617b0bae7b817f5f5ddb82021a
ThanhTan <nhattan425@gmail.com>
2025-09-23 21:02:53 +0700

    Remove commented-out API key settings for improved security and clarity

--- tests/test.py
+++ tests/test.py
@@ -5,9 +5,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from main import main
 
 # Set API key
-# os.environ['OPENAI_API_KEY'] = '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'
 # # groq_api_key
-# os.environ['GROQ_API_KEY'] = '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'
 
 if __name__ == "__main__":
     # Test English audio mode with EPDS
